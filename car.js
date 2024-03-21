349a6bf2fb59f8e1b92b41f4e8a8231f69803ae4
zoeLub <joesharrit45@gmail.com>
2024-03-21 21:04:01 +0200

    Added a make to the car, and added the commit from carEngine submodule.

--- car.js
+++ car.js
@@ -2,6 +2,7 @@ import Engine from './carEngine/engine.js';
 
 const Car = {
     engine: Engine,
+    make: "Ford",
     displayEngineInfo: function() {
         console.log("Car's Engine Information:");
         this.engine.displayInfo();
